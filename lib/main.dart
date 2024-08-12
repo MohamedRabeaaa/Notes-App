f702d68a356ba64c9dfa6fcd0614367e0f2bc2a3
MohamedRabeaaa <mohamedrabeaayed.gmail.com>
2024-08-12 13:50:11 +0300

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
+import "package:notes_app/Blocs/cubits/add_note_cubit/add_note_cubit.dart";
 import "package:notes_app/constants/constants.dart";
 import "package:notes_app/models/note_model.dart";
 import "views/empty_notes_view.dart";
@@ -16,13 +18,23 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Nunito',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        ),
+        // BlocProvider(
+        //   create: (context) => SubjectBloc(),
+        // ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Nunito',
+        ),
+        home: const EmptyNotesView(),
       ),
-      home: const EmptyNotesView(),
     );
   }
 }
